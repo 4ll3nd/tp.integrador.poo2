98ece7641ded67f574989380101138e1152706bb
Alexander Ferragut <alexanderferragut@gmail.com>
2025-06-02 10:20:39 -0300

    se formateo estructura

---
+++ test/ar/edu/unq/poo2/TpfinalTest/ClaseMuestraTest.java
@@ -0,0 +1,19 @@
+package ar.edu.unq.poo2.TpfinalTest;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+class ClaseMuestraTest {
+
+	@BeforeEach
+	void setUp() throws Exception {
+	}
+
+	@Test
+	void test() {
+		fail("Not yet implemented");
+	}
+
+}
