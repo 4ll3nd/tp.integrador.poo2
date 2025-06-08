c017d906e3d3568a91a764b1bf2c5833e9e924e9
Nicolás Bossi <nbossi@hotmail.com>
2025-06-08 20:49:55 -0300

    Se agrego la clase de Test OpinionTest

---
+++ test/ar/edu/unq/poo2/TpFinal/OpinionTest.java
@@ -0,0 +1,19 @@
+package ar.edu.unq.poo2.TpFinal;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+class OpinionTest {
+
+	@BeforeEach
+	void setUp() throws Exception {
+	}
+
+	@Test
+	void test() {
+		fail("Not yet implemented");
+	}
+
+}
