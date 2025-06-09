ca7c80c450c11c8820c806da5f258209c581bedd
Gimenez Ignacio Ariel <ariel_5195@hotmail.com>
2025-06-09 03:23:18 -0300

    Agrego Zona con getters y metodo de agregar muestra con sus test

---
+++ test/ar/edu/unq/poo2/TpFinal/ZonaDeCobertura.java
@@ -0,0 +1,48 @@
+package ar.edu.unq.poo2.TpFinal;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ZonaDeCobertura {
+
+	private String nombre;
+	private Ubicacion epicentro;
+	private double radio;
+	private List<Muestra> muestras;
+
+	public ZonaDeCobertura(String nombre, Ubicacion epicentro, double radio) {
+		this.nombre = nombre;
+		this.epicentro = epicentro;
+		this.radio = radio;
+		this.muestras = new ArrayList<Muestra>();
+	}
+
+	public String getNombre() {
+		return nombre;
+	}
+
+	public Ubicacion getEpicentro() {
+		return epicentro;
+	}
+
+	public double getRadio() {
+		return radio;
+	}
+
+	public List<Muestra> getMuestras() {
+		return muestras;
+	}
+
+	public void agregarMuestra(Muestra muestra) {
+		if (this.estaEnZona(muestra)) {
+			muestras.add(muestra);
+		}
+		
+	}
+
+	private boolean estaEnZona(Muestra muestra) {
+		
+		return epicentro.distancia( muestra.getUbicacion()) <= this.radio;
+	}
+
+}
---
+++ test/ar/edu/unq/poo2/TpFinal/ZonaDeCoberturaTest.java
@@ -0,0 +1,87 @@
+package ar.edu.unq.poo2.TpFinal;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.*;
+
+import java.util.List;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+class ZonaDeCoberturaTest {
+	private ZonaDeCobertura zone0;
+	private Ubicacion ubiMock;
+	private Ubicacion ubiMock2;
+	private Muestra muestraMock;
+
+	@BeforeEach
+	void setUp() throws Exception {
+
+		// Seteo de mocks
+		muestraMock = mock(Muestra.class);
+
+		// Mock de ubicaciones
+		ubiMock2 = mock(Ubicacion.class);
+		ubiMock = mock(Ubicacion.class);
+
+		// SUT Constructor
+		zone0 = new ZonaDeCobertura("Quilmes", ubiMock, 700d);
+	}
+
+	@Test
+	void testGetNombre() {
+		assertEquals("Quilmes", zone0.getNombre());
+	}
+
+	@Test
+	void testGetEpicentro() {
+		assertEquals(ubiMock, zone0.getEpicentro());
+	}
+
+	@Test
+	void testGetRadio() {
+		assertEquals(700d, zone0.getRadio());
+	}
+
+	@Test
+	void testMuestrasEnLaZonaRecienIniciada() {
+		assertEquals(List.of(), zone0.getMuestras());
+	}
+
+	@Test
+	void testAgregoUnaMuestraZonaNoCorrespondiente() {
+
+		// seteo los mocks
+		when(muestraMock.getUbicacion()).thenReturn(ubiMock2);
+		when(ubiMock.distancia(ubiMock2)).thenReturn(800d);
+
+		// lanzo el mensaje
+		zone0.agregarMuestra(muestraMock);
+
+		// Verifico si reciben los mensajes
+		verify(muestraMock).getUbicacion();
+		verify(ubiMock).distancia(ubiMock2);
+
+		// assert para saber si se agrego
+		assertEquals(0, zone0.getMuestras().size());
+
+	}
+
+	@Test
+	void testAgregoUnaMuestraZonaCorrespondiente() {
+		// seteo los mocks
+		when(muestraMock.getUbicacion()).thenReturn(ubiMock2);
+		when(ubiMock.distancia(ubiMock2)).thenReturn(400d);
+
+		// lanzo el mensaje
+		zone0.agregarMuestra(muestraMock);
+
+		// Verifico si reciben los mensajes
+		verify(muestraMock).getUbicacion();
+		verify(ubiMock).distancia(ubiMock2);
+
+		// assert para saber si se agrego
+		assertEquals(1, zone0.getMuestras().size());
+	}
+
+}
