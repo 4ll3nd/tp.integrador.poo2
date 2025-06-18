d72e99ec8a79a64a318b072bc345c67c0dee8e58
Nicolás Bossi <nbossi@hotmail.com>
2025-06-17 21:44:51 -0300

    Se agrego la interfaz de la funcionalidad externa de la organizacion

---
+++ src/ar/edu/unq/poo2/TpFinal/FuncionalidadExterna.java
@@ -0,0 +1,6 @@
+package ar.edu.unq.poo2.TpFinal;
+
+public interface FuncionalidadExterna {
+	
+	void nuevoEvento(OrganizacionNoGubernamental unaOrganizacion, ZonaDeCobertura zonaDeCobertura, Muestra unaMuestra);
+}
