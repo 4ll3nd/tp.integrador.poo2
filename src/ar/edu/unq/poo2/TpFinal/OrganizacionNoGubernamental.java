23031f90a7883f7733ebeea58ef8440ec0dc0e21
Nicolás Bossi <nbossi@hotmail.com>
2025-06-17 21:42:04 -0300

    Se testeo la funcionalidad para las funcionalidades externas de las organizaciones

--- src/ar/edu/unq/poo2/TpFinal/OrganizacionNoGubernamental.java
+++ src/ar/edu/unq/poo2/TpFinal/OrganizacionNoGubernamental.java
@@ -1,15 +1,19 @@
 package ar.edu.unq.poo2.TpFinal;
 
-public class OrganizacionNoGubernamental {
+public class OrganizacionNoGubernamental implements IObserverOrganizacion {
 
 	private int cantidadDeEmpleados;
 	private Ubicacion ubicacion;
 	private TipoDeOrganizacion tipoDeOrganizacion;
+	private FuncionalidadExterna funcionalidadDeAgregado;
+	private FuncionalidadExterna funcionalidadDeVerificacion;
 
-	public OrganizacionNoGubernamental(Ubicacion ubi, TipoDeOrganizacion tipOrga, int empleados) {
+	public OrganizacionNoGubernamental(Ubicacion ubi, TipoDeOrganizacion tipOrga, int empleados, FuncionalidadExterna funcionalidadDeAgregado, FuncionalidadExterna funcionalidadDeVerificacion) {
 		this.cantidadDeEmpleados = empleados;
 		this.ubicacion = ubi;
 		this.tipoDeOrganizacion = tipOrga;
+		this.funcionalidadDeAgregado = funcionalidadDeAgregado;
+		this.funcionalidadDeVerificacion = funcionalidadDeVerificacion;
 	}
 
 	public int getCantidadDeEmpleados() {
@@ -22,6 +26,48 @@ public class OrganizacionNoGubernamental {
 
 	public TipoDeOrganizacion getTipoDeOrganizacion() {
 		return tipoDeOrganizacion;
+	}
+
+	public void setFuncionalidadExternaNuevaMuestra(FuncionalidadExterna funcionalidad) {
+		
+		setFuncionalidadNuevaMuestra(funcionalidad);
+	}
+
+	private void setFuncionalidadNuevaMuestra(FuncionalidadExterna funcionalidad) {
+		
+		this.funcionalidadDeAgregado = funcionalidad;
+	}
+
+	public void setFuncionalidadExternaVerificacion(FuncionalidadExterna funcionalidad) {
+		
+		setFuncionalidadVerificacion(funcionalidad);
+	}
+
+	private void setFuncionalidadVerificacion(FuncionalidadExterna funcionalidad) {
+		
+		this.funcionalidadDeVerificacion = funcionalidad;
+	}
+
+	@Override
+	public void updateNuevaMuestra(Muestra muestra, ZonaDeCobertura zona) {
+		
+		getFuncionalidadDeAgregado().nuevoEvento(this, zona, muestra);
+	}
+
+	private FuncionalidadExterna getFuncionalidadDeAgregado() {
+
+		return this.funcionalidadDeAgregado;
+	}
+
+	@Override
+	public void updateVerificacionDeMuestra(Muestra muestra, ZonaDeCobertura zona) {
+		
+		getFuncionalidadDeVerificacion().nuevoEvento(this, zona, muestra);
+	}
+
+	private FuncionalidadExterna getFuncionalidadDeVerificacion() {
+		
+		return this.funcionalidadDeVerificacion;
 	}
 
 }
